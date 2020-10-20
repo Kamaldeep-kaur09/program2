f34762bb09f2d8829bf831ecc78c0959b58c1bf5
Kamaldeep Kaur <72203919+Kamaldeep-kaur09@users.noreply.github.com>
2020-10-20 09:46:03 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgeExceptionHandler
+{
+    class ProgramException
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int result = CheckAge();
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+            catch (CustomAge Age1)
+            {
+
+                Console.WriteLine(Age1.Message);
+            }
+
+
+        }
+        static int CheckAge()
+        {
+            Console.WriteLine("Enter age :");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age < 18)
+            {
+                throw new Exception("Age should be more than 18");
+            }
+            return age;
+        }
+
+    }
+    public class CustomAge : Exception
+    {
+        public CustomAge(string message) : base(message)
+        {
+
+        }
+    }
+}
+
